db829393639563694e7397949d0d30f8d32a8a4c
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2023-09-15 20:29:21 +0900

    use ReTestItems

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,4 @@
-using Test
+using ReTestItems: runtests, @testitem
+using Quantics: Quantics
 
-@testset "Quantics.jl" begin
-    include("util.jl")
-    include("tag.jl")
-    include("quanticsrepr.jl")
-    include("binaryop.jl")
-    include("fouriertransform.jl")
-    include("imaginarytime.jl")
-    include("mul.jl")
-    include("mps.jl")
-    include("transformer.jl")
-    include("grid.jl")
-end
-
-nothing
+runtests(Quantics)
