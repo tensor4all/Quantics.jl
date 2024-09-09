16c48e976e2b5eb3d7822c8ab9e42c7a8e3cf491
Markus Wallerberger <markus.wallerberger@tuwien.ac.at>
2024-09-09 22:47:11 +0200

    Affine transforms are now correct (mostly)

--- src/affine.jl
+++ src/affine.jl
@@ -35,6 +35,20 @@ function affine_transform_mpo(
     return mpo
 end
 
+"""
+    affine_transform_tensors(R, A, b)
+
+Compute vector of core tensors (constituent 4-way tensors) for a matrix product
+operator corresponding to the affine transformation `y = A*x + b`.
+"""
+function affine_transform_tensors(
+            R::Integer, A::AbstractMatrix{<:Integer},
+            b::AbstractVector{<:Integer})
+    M, N = size(A)
+    return affine_transform_tensors(
+            Int(R), SMatrix{M, N, Int}(A), SVector{M, Int}(b))
+end
+
 function affine_transform_tensors(
             R::Int, A::SMatrix{M, N, Int}, b::SVector{M, Int}
             ) where {M, N}
@@ -53,15 +67,11 @@ function affine_transform_tensors(
         # it out from the array
         bcurr = @. Bool(b & 1)
 
-        # Get tensor. For the last tensor, we ignore the carry and just
-        # or together all possible combinations.
-        new_carry, new_tensor = affine_transform_tensor(A, bcurr, carry)
-        if r == R
-            all_values = reduce(.|, eachslice(new_tensor, dims=1))
-            tensors[r] = reshape(all_values, 1, size(all_values)...)
-        else
-            tensors[r] = new_tensor
-        end
+        # Get tensor. For the last tensor, we discard the carry.
+        out_tf = (r == R) ? zero : identity
+        new_carry, data = affine_transform_core(A, bcurr, carry,
+                                                transform_out_carry=out_tf)
+        tensors[r] = data
 
         # Set carry to the next value
         carry = new_carry
@@ -70,26 +80,46 @@ function affine_transform_tensors(
     return tensors
 end
 
-function affine_transform_tensor(
+"""
+    core, out_carry = affine_transform_core(A, b, in_carry;
+                                            transform_out_carry=identity)
+
+Construct core tensor `core` for an affine transform.  The core tensor for an
+affine transformation is given by:
+
+    core[d, c, iy, ix] =
+        2 * out_carry[d] + y[iy] == A * x[ix] + b + in_carry[c]
+
+where `A`, a matrix of integers, and `b`, a vector of bits, which define the
+affine transform. `c` and `d` are indices into a set of integer vectors
+`in_carry` and `out_carry`, respectively, which encode the incoming and outgoing
+carry from the other core tensors. `x[ix] ∈ {0,1}^N` and `y[iy] ∈ {0,1}^M`
+are the binary input and output vectors, respectively, of the affine transform.
+They are indexed in a "little-endian" fashion.
+"""
+function affine_transform_core(
             A::SMatrix{M, N, Int}, b::SVector{M, Bool},
-            carry::AbstractVector{SVector{M, Int}}) where {M, N}
+            carry::AbstractVector{SVector{M, Int}};
+            transform_out_carry::Function=identity
+            ) where {M, N}
     # The basic idea here is the following: we compute r = A*x + b + c for all
     # "incoming" carrys d and all possible bit vectors, x ∈ {0,1}^N.  Then we
     # decompose r = 2*c + y, where y is again a bit vector, y ∈ {0,1}^M, and
     # c is the "outgoing" carry, which may be negative.  We then store this
     # as something like out[d][c, x, y] = true.
     out = Dict{SVector{M, Int}, Array{Bool, 3}}()
+    sizehint!(out, length(carry))
     for (c_index, c) in enumerate(carry)
         for (x_index, x) in enumerate(Iterators.product(ntuple(_ -> 0:1, N)...))
             r = A * SVector{N, Bool}(x) + b + SVector{N, Int}(c)
-            y = Bool.(r .& 1)
-            d = r .>> 1
+            y = @. Bool(r & 1)
+            d::SVector{M, Int} = transform_out_carry(r .>> 1)
             y_index = _spins_to_number(y) + 1
 
             d_mat = get!(out, d) do
                 return zeros(Bool, length(carry), 1 << M, 1 << N)
             end
-            d_mat[c_index, x_index, y_index] = true
+            @inbounds d_mat[c_index, x_index, y_index] = true
         end
     end
 
@@ -99,7 +129,7 @@ function affine_transform_tensor(
     value_out = Array{Bool, 4}(undef, length(out), length(carry), 1<<M, 1<<N)
     for (p_index, p) in enumerate(pairs(out))
         carry_out[p_index] = p.first
-        value_out[p_index, :, :, :] = p.second
+        value_out[p_index, :, :, :] .= p.second
     end
     return carry_out, value_out
 end
